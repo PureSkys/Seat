57875c6a66b31425c504034404fa6601a591fe06
Cyon <1900236964@qq.com>
2026-02-26 16:19:07 +0800

    fix: 移除路由和配置中的尾部斜杠以保持一致性

--- src/router/index.ts
+++ src/router/index.ts
@@ -2,7 +2,7 @@ import { createRouter, createWebHashHistory } from 'vue-router'
 import HomeView from '@/views/HomeView.vue'
 
 const router = createRouter({
-  history: createWebHashHistory('/seat/'),
+  history: createWebHashHistory('/seat'),
   routes: [
     {
       path: '/',
